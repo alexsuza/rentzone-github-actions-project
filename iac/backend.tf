f371d2c60dc5936aacfec34b684e4ddbd9d55f1f
alexsuza <alexdaysouza@gmail.com>
2023-08-18 14:12:07 -0600

    terraform code ready to test

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = alexsuza-terraform-state-github-actions
+    bucket         = "alexsuza-terraform-state-github-actions"
     key            = "rentzone-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "alexsuza-terraform-state-lock"
